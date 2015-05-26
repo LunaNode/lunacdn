e321f20b6fb875db1cdb7d78dbffe951516ace9d
Favyen Bastani <fbastani@perennate.com>
2015-05-25 20:21:03 -0400

    Use http.ServeContent for file downloads.
    We add a CacheReader that implements Reader/Seeker interfaces. This replaces the block caching with sub-goroutine code. ServeContent handles setting file type, content length, accept-ranges, and other stuff.

--- src/lunacdn/serve.go
+++ src/lunacdn/serve.go
@@ -5,6 +5,9 @@ import "fmt"
 import "strings"
 import "sync"
 import "time"
+import "os"
+import "errors"
+import "io"
 
 type Serve struct {
 	mu sync.Mutex
@@ -14,12 +17,88 @@ type Serve struct {
 	recentErrors map[string]time.Time
 }
 
+type CacheReader struct {
+	cache *Cache
+	serve *Serve
+	path string
+	file *CacheFile
+	offset int64
+
+	currentBlock []byte
+	currentBlockOffset int64
+}
+
+func MakeCacheReader(cache *Cache, serve *Serve, path string, file *CacheFile) *CacheReader {
+	this := new(CacheReader)
+	this.cache = cache
+	this.serve = serve
+	this.path = path
+	this.file = file
+	this.offset = 0
+	return this
+}
+
+func (this *CacheReader) Seek(offset int64, whence int) (int64, error) {
+	actualOffset := offset
+	if whence == os.SEEK_CUR {
+		actualOffset = this.offset + offset
+	} else if whence == os.SEEK_END {
+		actualOffset = this.file.Length + offset
+	}
+
+	if actualOffset < 0 {
+		return this.offset, errors.New("seek to negative offset")
+	}
+
+	this.offset = actualOffset
+	return this.offset, nil
+}
+
+func (this *CacheReader) Read(p []byte) (int, error) {
+	if this.offset >= this.file.Length {
+		return 0, io.EOF
+	}
+	Log.Debug.Printf("receive read request offset=%d len=%d", this.offset, len(p))
+
+	pOffset := 0
+	t := 0
+
+	for {
+		t++
+		if t > 5 {
+			os.Exit(1)
+		}
+		if this.currentBlock != nil && this.offset >= this.currentBlockOffset && this.offset < this.currentBlockOffset + int64(len(this.currentBlock)) {
+			copyBytes := copy(this.currentBlock[this.offset - this.currentBlockOffset:], p[pOffset:])
+			this.offset += int64(copyBytes)
+			pOffset += copyBytes
+		}
+
+		if pOffset < len(p) && this.offset < this.file.Length {
+			// need to read more bytes, load the next block
+			blockIndex := int(this.offset / BLOCK_SIZE)
+			this.currentBlock = this.cache.DownloadRead(this.file, blockIndex, true)
+			this.currentBlockOffset = int64(blockIndex) * BLOCK_SIZE
+
+			if this.currentBlock == nil {
+				// add to error cache since this download failed apparently
+				this.serve.reportError(this.path)
+				return 0, errors.New(fmt.Sprintf("failed to read block %d", blockIndex))
+			}
+		} else {
+			break
+		}
+	}
+
+	return pOffset, nil
+}
+
 func MakeServe(cfg *Config, cache *Cache) *Serve {
 	this := new(Serve)
 	this.cache = cache
 	this.recentErrors = make(map[string]time.Time)
 
-	http.HandleFunc("/", this.Handler)
+	http.HandleFunc("/", this.handler)
 	Log.Info.Printf("Starting HTTP server on [%s]", cfg.HttpBind)
 	err := http.ListenAndServe(cfg.HttpBind, nil)
 	if err != nil {
@@ -30,16 +109,16 @@ func MakeServe(cfg *Config, cache *Cache) *Serve {
 	return this
 }
 
-func (this *Serve) Handler(w http.ResponseWriter, r *http.Request) {
+func (this *Serve) handler(w http.ResponseWriter, r *http.Request) {
 	// convert from path like "/file" to "file"
 	path := r.URL.Path
 	shortPath := path[1 : len(path)]
 	pathParts := strings.Split(shortPath, "/")
 
 	// check if we recently error'd on this file, in which case show 500
-	noError := this.CheckError(shortPath)
+	noError := this.checkError(shortPath)
 	if !noError {
-		this.TemporarilyUnavailable(w)
+		this.temporarilyUnavailable(w)
 		Log.Debug.Printf("Request for [%s]: found in error cache", shortPath)
 		return
 	}
@@ -47,86 +126,23 @@ func (this *Serve) Handler(w http.ResponseWriter, r *http.Request) {
 	// grab the CacheFile object for reading
 	file := this.cache.DownloadInit(shortPath)
 	if file == nil {
-		this.NotFound(w)
+		this.notFound(w)
 		Log.Debug.Printf("Request for [%s]: file not found", shortPath)
 		return
 	}
 
-	// the file seems to exist, so let's try to read it block by block
-	// we read the blocks from our cache in a separate goroutine from where we write to the client
-	//  this is necessary since we don't want cache misses to stall client-server communication
-	// to synchronize, we:
-	//  a) use a buffered channel to send blocks from reader to writer
-	//      the size of the buffer is the maximum number of blocks to keep temporarily
-	//  b) the writer may terminate if there's an error; reader sees this by timing out
-	//      on reads and checking atomic boolean
-	Log.Debug.Printf("Request for [%s]: in progress", shortPath)
-	blockChannel := make(chan []byte, SERVE_BUFFER_BLOCKS)
-	terminated := make(chan bool, 1)
-
-	go func() {
-		blockIndex := 0
-		quit := false
-
-		for !quit {
-			block := this.cache.DownloadRead(file, blockIndex, true)
-			blockIndex++
-			written := false
-
-			for !written && !quit {
-				select {
-				case blockChannel <- block:
-					written = true
-				case <- terminated:
-					quit = true
-				}
-			}
-
-			if block == nil {
-				break
-			}
-		}
-	}()
-
-	firstBlock := true
-	var writtenLength int64 = 0
-	for {
-		block := <- blockChannel
-
-		// if this is the first empty block, we should 404, otherwise it probably means we're done so just quit
-		// if we see first non-empty block then set headers
-		if block == nil {
-			if firstBlock {
-				this.TemporarilyUnavailable(w)
-			}
-
-			// add to error cache if this download failed (didn't get all the bytes)
-			if writtenLength != file.Length {
-				Log.Warn.Printf("Failed to serve %s (len=%d but only wrote %d)", shortPath, file.Length, writtenLength)
-				this.mu.Lock()
-				this.recentErrors[shortPath] = time.Now()
-				this.mu.Unlock()
-			}
-			break
-		} else if firstBlock {
-			w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", pathParts[len(pathParts) - 1]))
-			w.Header().Set("Content-Type", "application/octet-stream")
-			w.Header().Set("Content-Length", fmt.Sprintf("%d", file.Length))
-			firstBlock = false
-		}
-
-		_, err := w.Write(block)
-		writtenLength += int64(len(block))
-
-		if err != nil {
-			Log.Debug.Printf("Failed to write block to HTTP: %s", err.Error())
-			terminated <- true
-			break
-		}
-	}
+	// pass to ServeContent to handle the complicated stuff
+	reader := MakeCacheReader(this.cache, this, shortPath, file)
+	http.ServeContent(w, r, pathParts[len(pathParts) - 1], time.Time{}, reader)
 }
 
-func (this *Serve) CheckError(path string) bool {
+func (this *Serve) reportError(path string) {
+	this.mu.Lock()
+	this.recentErrors[path] = time.Now()
+	this.mu.Unlock()
+}
+
+func (this *Serve) checkError(path string) bool {
 	this.mu.Lock()
 	defer this.mu.Unlock()
 
@@ -143,12 +159,12 @@ func (this *Serve) CheckError(path string) bool {
 	}
 }
 
-func (this *Serve) NotFound(w http.ResponseWriter) {
+func (this *Serve) notFound(w http.ResponseWriter) {
 	w.WriteHeader(404)
 	fmt.Fprint(w, "404")
 }
 
-func (this *Serve) TemporarilyUnavailable(w http.ResponseWriter) {
+func (this *Serve) temporarilyUnavailable(w http.ResponseWriter) {
 	w.WriteHeader(503)
 	fmt.Fprint(w, "503")
 }
